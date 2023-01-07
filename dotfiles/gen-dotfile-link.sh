1408478708a0a22edc0ba55df5881f0766a1db5d
tahy2201 <hyg121arx9@gmail.com>
2023-01-07 22:32:46 +0900

    fix gen link shell

--- dotfiles/gen-dotfile-link.sh
+++ dotfiles/gen-dotfile-link.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+ln -sf $SCRIPT_DIR/.zshrc ~/.zshrc
+ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
 
